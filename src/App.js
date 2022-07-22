dfef43bd8cd61dbcf95098701d36bafe8ff02cdb
cothi <jiungdev@gmail.com>
2022-07-22 16:47:34 +0900

    set layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import Sidebar from "./components/Sidebar"
+import Main from "./components/Main"
+import styles from "./App.module.css"
+
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleResize = () => {
+    if (window.innerWidth < 720) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  }
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  })
+
   return (
-    <div className="App">
-
-
-
+    <div className={styles.App}>
+      {
+        !isMobile ?
+          <Sidebar />
+          :
+          null
+      }
+      <Main />
     </div >
   );
 }
+
+
+
 
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import styles from "./Main.module.css"
+
+export default function Main() {
+  return (
+    <div className={styles.main_root}>
+      he
+      he
+    </div>
+  );
+}
